2eec3e5b4b9644c363515ede93db04e6d87389ab
Laurianelo <32616344+Laurianelo@users.noreply.github.com>
2020-05-08 17:43:54 +0200

    Scripts : player, left, right, up , down, Icommand

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    ICommand moveUp, moveRight, moveLeft, moveDown;
+
+
+    [SerializeField]
+    private float _speed = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Z))
+        {
+            //move up command
+            moveUp = new MoveUpCommand(this.transform, _speed);
+            moveUp.Execute();
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            //move down
+            moveDown = new MoveDownCommand(this.transform, _speed);
+            moveDown.Execute();
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            //move left
+            moveLeft = new MoveLeftCommand(this.transform, _speed);
+            moveLeft.Execute();
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            //move right
+            moveRight = new MoveRightCommand(this.transform, _speed);
+            moveRight.Execute();
+             
+        }
+
+
+    }
+}
